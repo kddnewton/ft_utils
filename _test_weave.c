670d20c9a009e87587c4f8aa8b87cad20661097b
Kevin Newton <kddnewton@gmail.com>
2024-12-26 12:56:47 -0500

    Test

--- _test_weave.c
+++ _test_weave.c
@@ -23,7 +23,7 @@ static PyObject* test_reset(
   destructor_called_2 = 0;
   tls_check_1 = 0;
   tls_check_2 = 0;
-  tls_1 = (void*) 0xDEADBEEF;
+  tls_1 = NULL; // (void*) 0xDEADBEEF;
   tls_2 = NULL;
   MUTEX_UNLOCK(destructor_mutex);
   Py_END_ALLOW_THREADS;
